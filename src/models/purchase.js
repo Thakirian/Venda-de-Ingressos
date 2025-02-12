d7233f9b606f17880a0fc54315e8f712d3eff2eb
Thaisse Kirian <thakirian@gmail.com>
2025-02-12 09:02:58 -0300

    create purchase

---
+++ src/models/purchase.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../data');
+
+const Compra = sequelize.define('Compra', {
+  quantidade: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: {
+        args: [1],
+        msg: 'A quantidade deve ser pelo menos 1'
+      }
+    }
+  },
+  precoTotal: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  }
+});
+
+module.exports = Compra;

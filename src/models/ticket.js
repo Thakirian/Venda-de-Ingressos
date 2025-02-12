3f4d48a92ec54703ee3416debb1c63d725a427a1
Thaisse Kirian <thakirian@gmail.com>
2025-02-12 09:03:17 -0300

    create ticket

---
+++ src/models/ticket.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../data');
+
+const Ingresso = sequelize.define('Ingresso', {
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  preco: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  quantidadeDisponivel: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: {
+        args: [0],
+        msg: 'A quantidade disponível não pode ser negativa'
+      }
+    }
+  }
+});
+
+module.exports = Ingresso;
